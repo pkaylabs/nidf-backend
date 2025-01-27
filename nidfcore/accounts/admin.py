cb7f1583876751c8986ef1a374f92a6f5ae60a47
princek <princesamuelpks@gmail>
2025-01-27 18:24:39 +0000

    update

--- nidfcore/accounts/admin.py
+++ nidfcore/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.site_header = 'NIDF Admin Portal'
+
+# user
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'church_profile__name','is_staff', 'is_superuser')
+    search_fields = ('name', 'email', 'phone',)
