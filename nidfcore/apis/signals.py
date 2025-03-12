1c96f2ead71db1d7edb2194706f3ed48bea0d44e
princek <princesamuelpks@gmail>
2025-03-12 18:48:47 +0000

    implemented generating and sending otp to users

--- nidfcore/apis/signals.py
+++ nidfcore/apis/signals.py
@@ -9,10 +9,14 @@ from accounts.models import User, OTP
 @receiver(post_save, sender=User)
 def create_otp(sender, instance, created, **kwargs):
     if created:
+        if isinstance.is_superuser or instance.phone_verified or instance.email_verified:
+            # do not create otp for superuser or already verified users
+            return
         otp = random.randint(1000, 9999)
         otp = OTP.objects.create(phone=instance.phone, otp=otp)
         print(f"OTP for {instance.email} is {otp}")
 
         # send otp
         otp.send_otp()
+        return
     
