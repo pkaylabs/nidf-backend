4811c291e58dd95c26cf7a6c814657ec4435b089
princek <princesamuelpks@gmail>
2025-03-12 19:22:01 +0000

    implemented otp verification

--- nidfcore/accounts/admin.py
+++ nidfcore/accounts/admin.py
@@ -14,7 +14,7 @@ class UserAdmin(admin.ModelAdmin):
 # church
 @admin.register(Church)
 class ChurchAdmin(admin.ModelAdmin):
-    list_display = ('name', 'pastor_name', 'district__name')
+    list_display = ('id', 'name', 'pastor_name', 'district__name')
     search_fields = ('name', 'pastor_name', 'district__name',)
 
 # district
